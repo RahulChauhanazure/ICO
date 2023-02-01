5cb3e9fd4d905b1f4c9c18abdc09b3e5e87987f2
Rahul Chauhan <rahulchauhan2k2@yahoo.com>
2023-01-31 22:21:26 -0500

    Initial ICO

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x80195AC099dB3e4c4DD643E4051F0fDe2f5c94da";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
